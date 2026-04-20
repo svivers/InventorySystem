68692bfd10353f4e8e1f64fce693817036a2032c
Svivers <thevanocherno@gmail.com>
2026-04-20 21:07:52 +0700

    Update ItemId.cs

--- Items/ItemId.cs
+++ Items/ItemId.cs
@@ -4,6 +4,7 @@ namespace Core.Items
 {
     public readonly struct ItemId : IEquatable<ItemId>
     {
+        // maybe later change to something more performant like uint or uuid
         private readonly string m_value;
 
         public ItemId(string value)
@@ -14,6 +15,8 @@ namespace Core.Items
         public string Value => m_value;
         public bool IsValid => m_value != null;
 
+        // overrides are needed store a struct in a dictionary without boxing plus a convinience of == != operators
+        
         public bool Equals(ItemId other) => m_value == other.m_value;
 
         public override bool Equals(object obj) => obj is ItemId other && Equals(other);
